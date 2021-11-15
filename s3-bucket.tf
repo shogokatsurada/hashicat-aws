6b259fc5f08de43f16a0e61cd9b027a675eccd92
Shogo Katsurada <shogokatsuada@mac.com>
2021-11-15 13:30:10 +0000

    Added s3 bucket module1

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/shogokatsurada1/s3-bucket/aws"
   version = "2.9.0"
-  # insert required variables here
+  # insert required variables here !!!
   buket_prefix = "${var.prefix}"
 }

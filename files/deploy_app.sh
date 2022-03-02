302664d3261ebcaae020f3f134e07f3a5162a3bd
Shogo Katsurada <shogokatsurada@mac.com>
2022-03-02 15:21:13 +0900

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Demo for NTTdocomo 2022/2/17
+  Welcome to ${PREFIX}'s app. Demo for Cisco CX.
   <!-- END -->
 
   </div>

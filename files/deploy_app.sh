5a9d7f13c38b0cd62a197fc0f64f5f009440e32a
Shogo Katsurada <shogokatsurada@mac.com>
2022-03-03 13:37:34 +0900

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Demo for NEC
+  Welcome to ${PREFIX}'s app. Demo for NEC 2022 3 3 13:37
   <!-- END -->
 
   </div>

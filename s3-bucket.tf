c12d45bd996c8de3d984a1b49dd99073afa0ed11
Shogo Katsurada <shogokatsurada@mac.com>
2022-06-15 15:42:50 +0900

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
-module "s3-bucket" {
-  source  = "app.terraform.io/shogokatsurada1/s3-bucket/aws"
-  version = "2.9.0"
-  # insert required variables here !!!
-  bucket_prefix = "${var.prefix}"
-}
+#module "s3-bucket" {
+#  source  = "app.terraform.io/shogokatsurada1/s3-bucket/aws"
+#  version = "2.9.0"
+#  # insert required variables here !!!
+#  bucket_prefix = "${var.prefix}"
+#}

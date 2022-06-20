fbb4ed721339e2d908c91608988cc01078d6744b
Shogo Katsurada <shogokatsurada@mac.com>
2022-06-21 06:45:50 +0900

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Demo for AWS.
+  Welcome to ${PREFIX}'s app. Demo for AWS 2022.
   <!-- END -->
 
   </div>

aa382ef75682132dd83aed45143a60e6efb52d91
Shogo Katsurada <shogo.katsurada@hashicorp.com>
2022-08-15 17:43:44 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -199,3 +199,10 @@ resource "aws_key_pair" "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
 }
+
+
+policy "restrict-all-vpc-traffic-acl-rules" {
+  source = "https://registry.terraform.io/v2/policies/hashicorp/aws-networking-terraform/1.0.2/policy/restrict-all-vpc-traffic-acl-rules.sentinel?checksum=sha256:b58e49dbd2e4795481c4af970aaf6dc883bdf5b6fb9cb47e331aa44883e8301d"
+  enforcement_level = "advisory"
+}
+  
